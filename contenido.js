5d859a02b2a45264fcc81afbeadab83ea0c07a87
DiarioPapa <corteslasso@gmail.com>
2026-02-25 15:08:27 -0500

    Update contenido.js

--- contenido.js
+++ contenido.js
@@ -21,3 +21,27 @@ Segundo párrafo.`
   ]
 
 };
+function mostrarFechaActual() {
+    const fecha = new Date();
+
+    const dias = [
+        "Domingo", "Lunes", "Martes", "Miércoles",
+        "Jueves", "Viernes", "Sábado"
+    ];
+
+    const meses = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio",
+        "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    ];
+
+    const diaSemana = dias[fecha.getDay()];
+    const dia = fecha.getDate();
+    const mes = meses[fecha.getMonth()];
+    const año = fecha.getFullYear();
+
+    const textoFecha = `${diaSemana} ${dia} de ${mes} de ${año}`;
+
+    document.getElementById("fecha-actual").innerText = textoFecha;
+}
+
+mostrarFechaActual();
