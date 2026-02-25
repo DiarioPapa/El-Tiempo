375e0f0e4d30d47d5aa72d2b0c6cc692d0fbeb27
DiarioPapa <corteslasso@gmail.com>
2026-02-25 14:53:16 -0500

    Create contenido.js

---
+++ contenido.js
@@ -0,0 +1,23 @@
+const noticias = {
+
+  portada: [
+    {
+      titulo: "Título aquí",
+      texto: `Primer párrafo aquí.
+
+Segundo párrafo aquí.
+
+Tercer párrafo aquí.`
+    }
+  ],
+
+  politica: [
+    {
+      titulo: "Título aquí",
+      texto: `Texto aquí.
+
+Segundo párrafo.`
+    }
+  ]
+
+};
